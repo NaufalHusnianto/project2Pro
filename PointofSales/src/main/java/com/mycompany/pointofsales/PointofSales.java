3ae329c16c6f059769524599c002b49ce1d85aba
Naufal Husnianto <husniantonaufal@gmail.com>
2023-11-29 09:33:19 +0700

    test

---
+++ PointofSales/src/main/java/com/mycompany/pointofsales/PointofSales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pointofsales;
+
+/**
+ *
+ * @author hp
+ */
+public class PointofSales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
